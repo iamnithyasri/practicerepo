aab7ec782bf304032c79dbff54ab8a9f9d069e8d
iamnithyasri <88226930+iamnithyasri@users.noreply.github.com>
2022-06-10 12:23:13 +0530

    add file

---
+++ Hello.java
@@ -0,0 +1,22 @@
+
+class Test{
+public static void main(String[ ]args){
+System.out.println("calling test class");
+}
+}
+
+
+
+
+
+class Hello{
+
+public static void main(String[ ]args){
+System.out.println("Hello World");
+System.out.print("Welcome to the world of java programming");
+System.out.println("language");
+}
+}
+
+
+
